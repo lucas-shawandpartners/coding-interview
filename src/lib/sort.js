f13b0b8aae1aff8b65c204d48926264c874698d4
Rafael da Silva Bonifácio <rsboni@gmail.com>
2020-12-15 20:15:24 -0300

    add rafael test

--- src/lib/sort.js
+++ src/lib/sort.js
@@ -1,8 +1,21 @@
 
 // TODO: Implement the bubble sort algorithm
-function bubbleSort(array) {
 
-    return array;
+function bubbleSort (array) {
+  let swapped = true
+  while (swapped) {
+    swapped = false
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        const before = array[i]
+        const after = array[i + 1]
+        array[i] = after
+        array[i + 1] = before
+        swapped = true
+      }
+    }
+  }
+  return array
 }
 
-module.exports = bubbleSort;
+module.exports = bubbleSort
--- src/server.js
+++ src/server.js
@@ -1,1 +1,30 @@
 // TODO: Implement the web server here.
+const express = require('express')
+const bubbleSort = require('./lib/sort')
+const path = require('path')
+
+//App conf
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
+// Router
+app.post('/', (req, res) => {
+  let { array } = req.body
+  array = array.split(',')
+  const sortedArray = bubbleSort(array)
+  res.send(sortedArray.join(', '))
+})
+
+app.get('/', (req, res) => {
+  res.render('index.ejs')
+})
+
+app.use(express.static(__dirname + '/'))
+
+app.listen(3000, () => {
+  console.log('serving in port 3000')
+})
